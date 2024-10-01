32cdc55cf40cce4b75ed19d078369e60089a53c1
yuna <yunaka.dev@gmail.com>
2024-10-02 01:31:29 +0900

    feat: add code highlighter and table

--- tools/github-markdown/PostEditor.js
+++ tools/github-markdown/PostEditor.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
-import ReactMarkdown from 'react-markdown';
+import React from 'react';
 import MdEditor from 'react-markdown-editor-lite';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import remarkGfm from 'remark-gfm';
 import 'react-markdown-editor-lite/lib/index.css';
 import './PostEditor.css';
 
-const PostEditor = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const handleTitleChange = (e) => setTitle(e.target.value);
-  const handleContentChange = ({ text }) => setContent(text);
+const PostEditor = ({ title, content, handleTitleChange, handleContentChange }) => {
 
+  const components = {
+    code: ({ node, inline, className, children, ...props }) => {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+        <SyntaxHighlighter style={materialDark} language={match[1]} PreTag="div" {...props}>
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    }
+  };
 
   return (
     <div className="container">
@@ -28,7 +41,11 @@ const PostEditor = () => {
         <MdEditor
           value={content}
           style={{ height: '500px' }}
-          renderHTML={(text) => <ReactMarkdown>{text}</ReactMarkdown>}
+          renderHTML={(text) => (
+            <ReactMarkdown components={components} remarkPlugins={[remarkGfm]}>
+              {text}
+            </ReactMarkdown>
+          )}
           onChange={handleContentChange}
         />
       </form>
--- tools/github-markdown/PostEditor.md
+++ tools/github-markdown/PostEditor.md
@@ -1,8 +1,13 @@
-## React 환경 설정
-
-### 라이브러리 설치
+# 1. 라이브러리 설치
 ```
 npm install react-markdown react-markdown-editor-lite
 npm install --save react-markdown-editor-lite@latest
 npm install --save marked
+
+// 코드 하이라이트 기능
+npm install highlight.js react-markdown react-syntax-highlighter
+
+// 표 기능
+npm install remark-gfm
 ```
+
