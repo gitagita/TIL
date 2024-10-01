9483ebf84bad65b1b20379569d2865b7751d08ee
yuna <yunaka.dev@gmail.com>
2024-10-01 20:57:26 +0900

    add markdown editor

---
+++ tools/github-markdown/PostEditor.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import MdEditor from 'react-markdown-editor-lite';
+import 'react-markdown-editor-lite/lib/index.css';
+import './styles.css';
+
+const PostEditor = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const handleTitleChange = (e) => setTitle(e.target.value);
+  const handleContentChange = ({ text }) => setContent(text);
+
+
+  return (
+    <div className="container">
+      <h1>게시글 작성</h1>
+      <form id="postForm">
+        <label htmlFor="title">제목:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={handleTitleChange}
+          required
+        />
+        <label htmlFor="content">내용:</label>
+        <MdEditor
+          value={content}
+          style={{ height: '500px' }}
+          renderHTML={(text) => <ReactMarkdown>{text}</ReactMarkdown>}
+          onChange={handleContentChange}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default PostEditor;
---
+++ tools/github-markdown/PostEditor.md
@@ -0,0 +1,8 @@
+## React 환경 설정
+
+### 라이브러리 설치
+```
+npm install react-markdown react-markdown-editor-lite
+npm install --save react-markdown-editor-lite@latest
+npm install --save marked
+```
